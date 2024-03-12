42d90653223e65e7e706fb43d21d72b6da8e8235
kamil@huggingface.co <kamilakesbi@gmail.com>
2024-03-12 15:08:10 +0000

    preprocess dataset function

--- hf_integration/preprocessing.py
+++ hf_integration/preprocessing.py
@@ -7,49 +7,63 @@ from hf_integration.dataset import concatenate
 from pyannote.audio.models.segmentation import PyanNet
 
 
-def prepare_chunk(file, start_time, duration, max_speakers_per_chunk=3):
+def get_labels_in_file(file):
 
-    end_time = start_time + duration
-    start_frame = math.floor(start_time * sample_rate)
-    num_frames = math.floor(duration * sample_rate)
-    end_frame = start_frame + num_frames
+    file_labels = []
+    for i in range(len(file["speakers"][0])):
 
-    waveform = file["audio"][start_frame:end_frame]
+        if file["speakers"][0][i] not in file_labels:
+            file_labels.append(file["speakers"][0][i])
+
+    return file_labels
+
+
+def get_segments_in_file(file, labels):
 
     file_annotations = []
 
-    file_labels = []
-    for i in range(len(file["timestamps_start"])):
+    for i in range(len(file["timestamps_start"][0])):
 
-        if file["speakers"][i] not in file_labels:
-            file_labels.append(file["speakers"][i])
-
-    for i in range(len(file["timestamps_start"])):
-
-        start_segment = file["timestamps_start"][i]
-        end_segment = file["timestamps_end"][i]
-        label = file_labels.index(file["speakers"][i])
+        start_segment = file["timestamps_start"][0][i]
+        end_segment = file["timestamps_end"][0][i]
+        label = labels.index(file["speakers"][0][i])
         file_annotations.append((start_segment, end_segment, label))
 
     dtype = [("start", "<f4"), ("end", "<f4"), ("labels", "i1")]
 
     annotations = np.array(file_annotations, dtype)
 
-    chunk_annotations = annotations[
-        (annotations["start"] < end_time) & (annotations["end"] > start_time)
+    return annotations
+
+
+def get_chunk(file, start_time, duration, max_speakers_per_chunk=3, sample_rate=16000):
+
+    end_time = start_time + duration
+    start_frame = math.floor(start_time * sample_rate)
+    num_frames = math.floor(duration * sample_rate)
+    end_frame = start_frame + num_frames
+
+    waveform = file["audio"][0][start_frame:end_frame]
+
+    labels = get_labels_in_file(file)
+
+    file_segments = get_segments_in_file(file, labels)
+
+    chunk_segments = file_segments[
+        (file_segments["start"] < end_time) & (file_segments["end"] > start_time)
     ]
 
     model = PyanNet(sincnet={"stride": 10})
     step = model.receptive_field.step
     half = 0.5 * model.receptive_field.duration
 
-    start = np.maximum(chunk_annotations["start"], start_time) - start_time - half
+    start = np.maximum(chunk_segments["start"], start_time) - start_time - half
     start_idx = np.maximum(0, np.round(start / step)).astype(int)
 
-    end = np.minimum(chunk_annotations["end"], end_time) - start_time - half
+    end = np.minimum(chunk_segments["end"], end_time) - start_time - half
     end_idx = np.round(end / step).astype(int)
 
-    labels = list(np.unique(chunk_annotations["labels"]))
+    labels = list(np.unique(chunk_segments["labels"]))
     num_labels = len(labels)
 
     if num_labels > max_speakers_per_chunk:
@@ -60,12 +74,41 @@ def prepare_chunk(file, start_time, duration, max_speakers_per_chunk=3):
 
     mapping = {label: idx for idx, label in enumerate(labels)}
 
-    for start, end, label in zip(start_idx, end_idx, chunk_annotations["labels"]):
+    for start, end, label in zip(start_idx, end_idx, chunk_segments["labels"]):
 
         mapped_label = mapping[label]
         y[start : end + 1, mapped_label] = 1
 
-    return waveform, y
+    return waveform, y, labels
+
+
+def get_start_positions(file, duration, overlap, sample_rate=16000):
+
+    file_duration = len(file["audio"][0]) / sample_rate
+    start_positions = np.arange(0, file_duration, duration * (1 - overlap))
+
+    return start_positions
+
+
+def chunk_file(file, duration=2, overlap=0.5):
+
+    new_batch = {
+        "input": [],
+        "target": [],
+        "label": [],
+    }
+
+    start_positions = get_start_positions(file, duration, overlap)
+
+    for start_time in start_positions:
+
+        X, y, label = get_chunk(file, start_time, duration)
+
+        new_batch["input"].append(X)
+        new_batch["target"].append(y)
+        new_batch["label"].append(label)
+
+    return new_batch
 
 
 if __name__ == "__main__":
@@ -74,16 +117,11 @@ if __name__ == "__main__":
 
     dataset = ds.filter(lambda x: x["meeting_id"] == "EN2001a")
     dataset = dataset.sort("begin_time")
-    dataset = dataset.select(range(320))
-    result = dataset.map(
+    dataset = dataset.select(range(32))
+    dataset = dataset.map(
         concatenate, batched=True, batch_size=32, remove_columns=dataset.column_names
     )
 
-    sample_rate = 16000
-    overlap = 0.25
-    start_time = 30
-    duration = 10
-    file = result[0]
-
-    waveform, y = prepare_chunk(file, start_time, duration, max_speakers_per_chunk=3)
-    print(waveform, y)
+    processed_dataset = dataset.map(
+        chunk_file, batched=True, batch_size=1, remove_columns=dataset.column_names
+    )
