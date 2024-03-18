a87eb7619a41b879755e71fb05605ce25375e1e6
kamil@huggingface.co <kamilakesbi@gmail.com>
2024-03-18 15:25:01 +0000

    update metric computation

--- hf_integration/metrics.py
+++ hf_integration/metrics.py
@@ -1,56 +1,12 @@
-import numpy as np
 import torch
 
-from pyannote.audio.utils.permutation import permutate
+from pyannote.audio.torchmetrics import (
+    DiarizationErrorRate,
+    FalseAlarmRate,
+    MissedDetectionRate,
+    SpeakerConfusionRate,
+)
 from pyannote.audio.utils.powerset import Powerset
-
-
-def discrete_diarization_error_rate(reference: np.ndarray, hypothesis: np.ndarray):
-    """Discrete diarization error rate
-
-    Parameters
-    ----------
-    reference : (num_frames, num_speakers) np.ndarray
-        Discretized reference diarization.
-        reference[f, s] = 1 if sth speaker is active at frame f, 0 otherwise
-    hypothesis : (num_frames, num_speakers) np.ndarray
-        Discretized hypothesized diarization.
-       hypothesis[f, s] = 1 if sth speaker is active at frame f, 0 otherwise
-
-    Returns
-    -------
-    der : float
-        (false_alarm + missed_detection + confusion) / total
-    components : dict
-        Diarization error rate components, in number of frames.
-        Keys are "false alarm", "missed detection", "confusion", and "total".
-    """
-
-    reference = reference.astype(np.half)
-    hypothesis = hypothesis.astype(np.half)
-
-    # permutate hypothesis to maximize similarity to reference
-    (hypothesis,), _ = permutate(reference[np.newaxis], hypothesis)
-
-    # total speech duration (in number of frames)
-    total = 1.0 * np.sum(reference)
-
-    # false alarm and missed detection (in number of frames)
-    detection_error = np.sum(hypothesis, axis=1) - np.sum(reference, axis=1)
-    false_alarm = np.maximum(0, detection_error)
-    missed_detection = np.maximum(0, -detection_error)
-
-    # speaker confusion (in number of frames)
-    confusion = np.sum((hypothesis != reference) * hypothesis, axis=1) - false_alarm
-
-    false_alarm = np.sum(false_alarm)
-    missed_detection = np.sum(missed_detection)
-    confusion = np.sum(confusion)
-
-    if total != 0:
-        return (false_alarm + missed_detection + confusion) / total
-    else:
-        return 0
 
 
 class Metrics:
@@ -60,24 +16,43 @@ class Metrics:
         self.classes = specifications.classes
         self.powerset_max_classes = specifications.powerset_max_classes
 
+        self.model_powerset = Powerset(
+            len(self.classes),
+            self.powerset_max_classes,
+        )
+
+        self.metrics = {
+            "der": DiarizationErrorRate(0.5),
+            "confusion": SpeakerConfusionRate(0.5),
+            "missed_detection": MissedDetectionRate(0.5),
+            "false_alarm": FalseAlarmRate(0.5),
+        }
+
     def der_metric(self, eval_pred):
 
         logits, labels = eval_pred
 
         if self.powerset:
-            conversion = Powerset(len(self.classes), self.powerset_max_classes)
-            logits = conversion(torch.tensor(logits)).cpu().numpy()
+            predictions = self.model_powerset.to_multilabel(torch.tensor(logits))
+        else:
+            predictions = torch.tensor(logits)
 
-        # Argmax ?
-        predictions = (logits >= 0.5).astype(np.float32)
+        labels = torch.tensor(labels)
 
-        metric = 0
-        for i in range(len(predictions)):
-            prediction = predictions[i]
-            label = labels[i]
+        predictions = torch.transpose(predictions, 1, 2)
+        labels = torch.transpose(labels, 1, 2)
 
-            metric += discrete_diarization_error_rate(label, prediction)
+        metrics = {"der": 0, "false_alarm": 0, "missed_detection": 0, "confusion": 0}
 
-        metric /= len(predictions)
+        metrics["der"] += self.metrics["der"](predictions, labels).cpu().numpy()
+        metrics["false_alarm"] += (
+            self.metrics["false_alarm"](predictions, labels).cpu().numpy()
+        )
+        metrics["missed_detection"] += (
+            self.metrics["missed_detection"](predictions, labels).cpu().numpy()
+        )
+        metrics["confusion"] += (
+            self.metrics["confusion"](predictions, labels).cpu().numpy()
+        )
 
-        return {"der": metric}
+        return metrics
